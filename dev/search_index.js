d07fa4b0a8979118d82e91abe025fdae2bb5d652
Documenter.jl <documenter@juliadocs.github.io>
2022-02-09 00:34:33 +0000

    build based on 6bd1c03

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ITensorParallel","category":"page"},{"location":"#ITensorParallel","page":"Home","title":"ITensorParallel","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ITensorParallel.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ITensorParallel]","category":"page"}]
+}
