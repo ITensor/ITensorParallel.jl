c67964fb3e5369bfeb2ad15e7a1e771e7e255b31
Matthew Fishman <mfishman@flatironinstitute.org>
2022-05-12 08:41:42 -0400

    Add MPI parallelized ProjMPOSum

--- src/ITensorParallel.jl
+++ src/ITensorParallel.jl
@@ -1,6 +1,7 @@
 module ITensorParallel
 
 using Distributed
+using MPI
 using ITensors
 
 import ITensors: product, position!, noiseterm, lproj, rproj
@@ -9,7 +10,8 @@ include("default_in_partition.jl")
 include("opsum_sum.jl")
 include("threaded_projmposum.jl")
 include("distributed_projmposum.jl")
+include("mpi_projmposum.jl")
 
-export ThreadedProjMPOSum, opsum_sum, DistributedSum
+export ThreadedProjMPOSum, opsum_sum, DistributedSum, MPISum
 
 end
