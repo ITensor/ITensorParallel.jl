9d357dee53d7fa78e950595dd176a00c6fc1b964
mtfishman <mtfishman@users.noreply.github.com>
2022-08-06 02:05:33 +0000

    Format .jl files

--- examples/hubbard_conserve_momentum/split_vec.jl
+++ examples/hubbard_conserve_momentum/split_vec.jl
@@ -6,6 +6,8 @@ function split_vec(x::Vector, npartitions::Int)
     which_part = mod1(which_part + 1, npartitions)
   end
   partition_stops = [0; accumulate(+, partition_lengths)]
-  partition_ranges = [(partition_stops[j] + 1):partition_stops[j + 1] for j in 1:(length(partition_stops) - 1)]
+  partition_ranges = [
+    (partition_stops[j] + 1):partition_stops[j + 1] for j in 1:(length(partition_stops) - 1)
+  ]
   return [x[partition_range] for partition_range in partition_ranges]
 end
