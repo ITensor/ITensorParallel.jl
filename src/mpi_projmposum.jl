68d75310099921f9d55b341f3500bba4c59f8aed
Benedikt Kloss <bkloss@flatironinstitute.org>
2023-01-19 12:54:14 -0500

    Remove brittle _Allreduce implementation.

--- src/mpi_projmposum.jl
+++ src/mpi_projmposum.jl
@@ -14,50 +14,13 @@ end
 
 MPISum(data::T, comm=MPI.COMM_WORLD) where {T} = MPISum{T}(data, comm)
 
-nsite(P::MPISum) = ITensors.nsite(P.data)
+nsite(P::MPISum) = nsite(P.data)
 
 Base.length(P::MPISum) = length(P.data)
 
-function _Allreduce(sendbuf::ITensor, op, comm)
-  return _Allreduce(NDTensors.storagetype(tensor(sendbuf)), sendbuf, op, comm)
-end
-
-function _Allreduce(::Type{<:BlockSparse}, sendbuf::ITensor, op, comm)  @deprecate
-  # Similar to:
-  #
-  # recvbuf = ITensor(eltype(sendbuf), flux(sendbuf), inds(sendbuf))
-  #
-  # But more efficient.
-  nprocs = MPI.Comm_size(comm)
-  N = order(sendbuf)
-  @show MPI.Comm_rank(comm), nnzblocks(sendbuf)
-  max_nblocks = maximum(MPI.Allgather(nnzblocks(sendbuf), comm))
-  blocks_sendbuf = fill(0, max_nblocks, N)
-  nzblocks_matrix = Matrix{Int}(reduce(hcat, collect.(nzblocks(sendbuf)))')
-  blocks_sendbuf[1:size(nzblocks_matrix, 1), :] = nzblocks_matrix
-  allblocks = MPI.Allgather(blocks_sendbuf, comm)
-  allblocks_tensor = reshape(allblocks, max_nblocks, N, nprocs)
-  allblocks_vector = [allblocks_tensor[:, :, i] for i in 1:nprocs]
-  allblocks_vector_tuple = [
-    [Tuple(allblocks_vector[n][i, :]) for i in 1:max_nblocks] for n in 1:nprocs
-  ]
-  blocks = Block.(filter(≠(ntuple(Returns(0), N)), union(allblocks_vector_tuple...)))
-  recvbuf = itensor(BlockSparseTensor(eltype(sendbuf), blocks, inds(sendbuf)))
-  sendbuf_filled = copy(recvbuf)
-  sendbuf_filled .= sendbuf
-  MPI.Allreduce!(sendbuf_filled, recvbuf, +, comm)
-  return recvbuf
-end
-
-function _Allreduce(::Type{<:Dense}, sendbuf::ITensor, op, comm)
-  recvbuf = similar(sendbuf)
-  MPI.Allreduce!(sendbuf, recvbuf, op, comm)
-  return recvbuf
-end
-
 function _allreduce(sendbuf,op,comm::MPI.Comm)
   ##maybe better to implement as allgather with local reduce, but higher communication cost associated
-  bufs=MPI.gather(sendbuf,0,comm)
+  bufs=_gather(sendbuf,0,comm)
   rank=MPI.Comm_rank(comm)
   if rank==0
     if op==+
